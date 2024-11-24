f982a1c7cae587379be5af179fcf8d502f473b0e
Rocco Vaccone <rocco@vaccone.us>
2024-11-24 17:04:07 -0500

    Added nvim-surround template

---
+++ plugins/kylechui#nvim-surround.lua
@@ -0,0 +1,11 @@
+return {
+    source = "kylechui/nvim-surround",
+    _metadata = {
+        name = "nvim-surround",
+        config_name = "nvim-surround",
+        description = "Add/change/delete surrounding delimiter pairs with ease",
+        homepage = "https://github.com/kylechui/nvim-surround",
+        pre_setup = [[version = "*",
+        event = "VeryLazy",]]
+    }
+}
